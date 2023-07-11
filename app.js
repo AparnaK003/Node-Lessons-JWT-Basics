8d7d1a4a3d0c29f38dd69155ebf9689467ec8c89
Aparna <aparnakishore003@gmail.com>
2023-07-11 12:15:00 +0530

    JWT basics project

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+require("express-async-errors"); // async wrapper
+
+const express = require("express");
+const app = express();
+
+const mainRouter = require("./routes/main"); // equivalent to import
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.static("./public")); // frontend part
+app.use(express.json());
+
+app.use("/api/v1", mainRouter); // route => server side
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
